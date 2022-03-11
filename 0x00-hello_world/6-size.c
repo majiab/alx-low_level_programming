a655f21ba5a883909d5466be06df792f56431ae1
majiab <a1b2e5netasnake@gmail.com>
2022-03-11 15:35:42 +0300

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,15 @@ include <stdio.h>
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
+char a;
+int b=10;
+long int c;
+long long int d;
 float f;
-
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
+printf("Size of a char: %d byte(s) \n",sizeof(a));
+printf("Size of an int: %d byte(s) \n",sizeof(b));
+printf("Size of a long int: %d byte(s) \n",sizeof(c));
+printf("Size of a long long int: %d byte(s) \n",sizeof(d));
+printf("Size of a float: %d byte(s) \n",sizeof(f));
 return (0);
 }
