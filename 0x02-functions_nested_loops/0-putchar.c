2bba1febdfba60419b9dcc6ecc77de6a82f6e46f
majiab <a1b2e5netasnake@gmail.com>
2022-03-15 20:32:35 +0300

    put character using main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,14 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+
+int main(void)
 {
-return (write(1, &c, 1));
+char h = "_putchar";
+	
+
+_putchar(h);
+_putchar('\n');
+return (0);
 
 }
