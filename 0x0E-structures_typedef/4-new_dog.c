7d8b005717a9cf31e87721a394f27edc8bb4bbdf
majiab <a1b2e5netasnake@gmail.com>
2022-04-12 15:15:14 +0300

    free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,9 +21,9 @@ return (NULL);
 }
 
 for (new_name = 0 ; name[new_name] ; new_name++)
-		
+		;
 for (new_owner = 0 ; owner[new_owner] ; new_owner++)
-		
+		;
 
 new_dog->name = malloc(new_name + 1);
 new_dog->owner = malloc(new_owner + 1);
@@ -38,11 +38,11 @@ return (NULL);
 
 for (i = 0 ; i < new_name ; i++)
 new_dog->name[i] = name[i];
-new_dog->name[i] = '\0';
+new_dog->name[i] = '0';
 new_dog->age = age;
 
 for (i = 0 ; i < new_owner ; i++)
 new_dog->owner[i] = owner[i];
-new_dog->owner[i] = '\0';
+new_dog->owner[i] = '0';
 return (new_dog);
 }
