8123e4d52cf92fd8a7190b17bb7899c8cca247ce
majiab <a1b2e5netasnake@gmail.com>
2022-04-29 10:49:31 +0300

    all data

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,13 +4,13 @@
  * @n: a string of 0 and 1 chars
  *
  */
-void print_binary(unsigned long int n)
+void print_binary(unsigned long int n )
 {
-int printbin = ((n & 1) + 0);
+int printbin = (( n & 1 ) + 0 );
 
-if (n > 1)
+if ( n > 1)
 {
-print_binary(n >> 1);
+print_binary( n >> 1 );
 }
 _putchar(printbin);
 }
