209b99f78fe9d56872d1f0833a5e0ecd4e8a2f71
majiab <a1b2e5netasnake@gmail.com>
2022-04-29 10:59:51 +0300

    all data

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,16 +1,46 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_binary - prints the binary representation of a number
- * @n: a string of 0 and 1 chars
- *
+ * print_binary - prints decimal as binary
+ * @n: long integer
  */
-void print_binary(unsigned long int n )
-{
-int printbin = (( n & 1 ) + 0 );
 
-if ( n > 1)
+void print_binary(unsigned long int n)
 {
-print_binary( n >> 1 );
+signed long int size;
+char c;
+int flag;
+
+size = sizeof(n) * 8 - 1;
+
+if (n == 0)
+{
+printf("0");
+return;
 }
-_putchar(printbin);
+
+if (n == 1)
+{
+printf("1");
+return;
+}
+
+flag = 0;
+
+while (size >= 0)
+{
+c = (n >> size) & 1;
+
+if (flag == 1)
+putchar(c + '0');
+else
+{
+if (c == 1)
+{
+putchar(c + '0');
+flag = 1;
+}
+}
+
+size -= 1;
+}
 }
